c1ee6be7ac0333b0b3bec22f52adc71343a0243e
jef210 <jmalek210@gmail.com>
2015-04-14 20:05:29 -0600

    added media queries and links

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width">
+    <meta property="og:image" content="/assets/images/hyperionlogobigx.png"/>
     <title><?php bloginfo('name'); ?></title>
     <?php wp_head(); ?>
 
