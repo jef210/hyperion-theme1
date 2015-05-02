27c982d49aace22d8b299cfdd7489881adf2055e
jef210 <jmalek210@gmail.com>
2015-05-01 20:21:00 -0600

    added popups and responsiveness along with all images

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if (have_posts()):
         <div class="top-text-box col-sm-12 col-md-12 col-lg-12">
         <div id="top-logo"><img src="/wordpress/wp-content/themes/hyperion_theme1/assets/images/hyperion-logo.svg"/></div>
         <div id="hyperion-blurb">
-            <p><strong>HYPERION WHOLESALE </strong>is privileged to be a full service DSD and Wholesale distributor in the Front Range of Colorado.  We pride ourselves in our top-notch customer service, our product guarantees, and the product brands we represent.</p>
+            <p class="shadow-text"><strong>HYPERION WHOLESALE </strong>is privileged to be a full service DSD and Wholesale distributor in the Front Range of Colorado.  We pride ourselves in our top-notch customer service, our product guarantees, and the product brands we represent.</p>
 <div class="btn-group">
   <button class="btn btn-default btn-lg dropdown-toggle btn1" type="button" data-toggle="dropdown" aria-expanded="false">
   <h1>Contact</h1> 
@@ -43,23 +43,24 @@ if (have_posts()):
  
 
  <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-   <button type="button" class="btn btn-default brand-box brand-logo" id="lone-star-logo" data-container="body" data-toggle="popover" data-placement="bottom" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">
+   <button type="button" class="btn btn-default brand-box brand-logo popover-with-html" id="lone-star-logo" data-container="body" data-toggle="popover" data-placement="bottom" title="The History of Lone Star:" data-html="true" data-content="
+An avid outdoorsmen and master chef in his free time, Lonestar Western Beef’s founder and President, John Bachman, was persistently encouraged by numerous friends and family to turn his dabbling in the creation of venison jerky into a business that would allow the public to enjoy his amazing jerky. After retiring from the Air Force, John decided to follow the persuasions of his friends and family and his dream of owning his own company by forming Lone Star Western Beef Inc." data-trigger="focus" >
    </button>
    </div> 
 
    <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-     <button type="button" class="btn btn-default brand-box brand-logo" id="sw-jerky-logo" data-container="body" data-toggle="popover" data-placement="bottom" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">
+     <button type="button" class="btn btn-default brand-box brand-logo" id="sw-jerky-logo" data-container="body" data-toggle="popover" data-placement="bottom" title="About Sweetwood Jerky:" data-content="Sweetwood Cattle Company is steeped rich in history and tradition in Steamboat Springs, Colorado USA. The ranch is located in the Elk River valley, which was visited by the Ute Indians annually in the summertime, because of its plentiful hunting and medicinal hot springs. Cattle were introduced to the area in 1860 thus, the beginning of Routt County’s agriculture history. In 1928 a fully operational cattle company was established at the ranch headquarters. Many winters and summers have come and gone, and Sweetwood Cattle Company still drives on the tradition." data-trigger="focus">
      </button>
      </div> 
 
     <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-     <button type="button" class="btn btn-default brand-box brand-logo" id="solf-logo" data-container="body" data-toggle="popover" data-placement="bottom" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">
+     <button type="button" class="btn btn-default brand-box brand-logo" id="solf-logo" data-container="body" data-toggle="popover" data-placement="bottom" title="About Snack Out Loud:" data-content="We were tired of having to choose between what we want to eat & what we know we should eat. So we created a snack that we can love without the guilt, a snack that leaves us satisfied for hours & does a body good. It’s simple, really. We started with one of the world’s most abundant and nutrient-dense superfoods – beans – then roasted and seasoned them to crunchy perfection." data-trigger="focus">
      </button>
        
      </div>
 
     <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-     <button type="button" class="btn btn-default brand-box brand-logo" id="buffalo-bills-logo" data-container="body" data-toggle="popover" data-placement="bottom" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">
+     <button type="button" class="btn btn-default brand-box brand-logo" id="buffalo-bills-logo" data-container="body" data-toggle="popover" data-placement="bottom" title="About Buffalo Bill's Jerky:" data-content="Honest snacks made with real meat and no filler. In all the flavors you love. That’s what you’ll find here at Buffalo Bills. It all started with our premium hickory beef jerky, still our customers’ favorite. We couldn’t stop, and our customers couldn’t get enough. So today we make more than 65 types of beef jerky and 25 types of beef sticks. Plus we have turkey jerky, venison jerky, ham sticks, turkey sticks, and venison sticks." data-trigger="focus">
      </button>
        
      </div>
@@ -74,25 +75,29 @@ if (have_posts()):
  
 
   <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-     <button type="button" class="btn btn-default brand-box brand-logo" id="olomomo-logo" data-container="body" data-toggle="popover" data-placement="bottom" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">
+     <button type="button" class="btn btn-default brand-box brand-logo" id="olomomo-logo" data-container="body" data-toggle="popover" data-placement="bottom" title="About Olomomo Nuts: "data-content="Eating OLOMOMO nuts will likely make you nuttier, happier and more adventurous. It’s true. Studies have shown that when individuals eat delicious nuts made by a super monkey they were 10 times more likely to invent something awesome, live to be 100 (but look like they’re 30) and… well, of course we might be exaggerating a little bit, but it’s common knowledge that nuts are pretty awesome for you.  Ours just might be the best you’ve ever tasted.  Some things are best experienced yourself, eh? Spice up your day a little." data-trigger="focus">
      </button>
        
      </div> 
 
   <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-     <button type="button" class="btn btn-default brand-box brand-logo" id="wild-zora-logo" data-container="body" data-toggle="popover" data-placement="bottom" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">
+     <button type="button" class="btn btn-default brand-box brand-logo" id="wild-zora-logo" data-container="body" data-toggle="popover" data-placement="bottom" title="About Wild Zora: " data-content="In early 2011 our family started to learn about Gluten-Free, Paleo, and Primal food. We just didn’t feel good about feeding the typical sugary, grain-filled snacks on the market to our kids! Jerky proved no better. So many jerky snacks contain nitrites, nitrates and MSG, and all of them are just too tough and too salty. When we’re hungry, our bodies crave lasting energy: something substantial, savory, and nutritious. Nothing on the market lived up to that standard.
+
+    Beginning in 2013, we created our own alternative. At home, we always considered meat AND vegetables a complete meal. So we started to mix and dehydrate 100% grass-fed beef from a local rancher along with fresh vegetables from our garden.  We discovered that we could make super tasty meat and veggie snacks that our family really enjoyed!
+
+    Our kids started sharing our beef and veggie snacks with their friends at school. Soon many of our friends insisted we start a business to sell them. We listened! We now make our meat and veggie bars in our own fully USDA-certified kitchen just outside Fort Collins, Colorado." data-trigger="focus">
      </button>
        
      </div> 
 
      <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-     <button type="button" class="btn btn-default brand-box brand-logo" id="rm-popcorn-logo" data-container="body" data-toggle="popover" data-placement="bottom" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">
+     <button type="button" class="btn btn-default brand-box brand-logo" id="rm-popcorn-logo" data-container="body" data-toggle="popover" data-placement="bottom" title="About Rocky Mountain Popcorn: " data-content="The company was born from a passionate commitment to small-batch-crafted taste on a go-anywhere scale. And whether a customer is making a quick grocery run or grabbing a treat before hopping on a plane, we believe NOBODY should have to compromise quality and freshness for convenience. With Open Road Snacks™ you can rest assured you’re doing better. For you." data-trigger="focus">
      </button>
        
      </div> 
 
    <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-     <button type="button" class="btn btn-default brand-box brand-logo" id="horseshoe-logo" data-container="body" data-toggle="popover" data-placement="bottom" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">
+     <button type="button" class="btn btn-default brand-box brand-logo" id="horseshoe-logo" data-container="body" data-toggle="popover" data-placement="bottom" title="About Ricky's Lucky Nuts: " data-content="At Ricky's Lucky Nuts, our philosophy as a nut company is not to produce just another packaged snack product, but to make a flavored peanut that is truly a nut like no other. This is why our roasted nuts are all-natural and healthy with no artificial ingredients of any kind—specialty nuts that create a memorable taste experience from honest, unadulterated ingredients. If we cannot pronounce an ingredient, we don't want it! In fact, we urge you to take a peek at our ingredients list to see how we utilize many of the same items you would find in your own kitchen cupboard. Plus, our sweet and salty nuts are GMO-free." data-trigger="focus">
      </button>
        
      </div>
@@ -108,13 +113,15 @@ if (have_posts()):
                          
  
 
-  <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
-     <button type="button" class="btn btn-default brand-box brand-logo" id="gsl-logo" data-container="body" data-toggle="popover" data-placement="bottom" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">
+  <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+     <button type="button" class="btn btn-default brand-box brand-logo" id="gsl-logo" data-container="body" data-toggle="popover" data-placement="bottom" data-content="Ever thought about the effect smoking has on your planet, beyond just the carcinogenic smoke released into the air after taking a drag? After all, what significance could one tiny cigarette have on the environment? Probably not much you'd think, but add up the roughly one billion smokers and 12.3 billion cigarettes that are smoked every day, the results may be surprising.
+
+    Truth is, the tobacco industry's environmental impact reaches far beyond the cigarettes people smoke every day. That's why GreenSmartLiving has put together a visual snapshot of the true impact tobacco has on the world, from production to consumption. See it all for yourself." data-trigger="focus">
      </button>
        
      </div>
 
-   <div class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
+   <div class="col-xs-12 col-sm-6 col-md-9 col-lg-9">
       <div class="bottom-logo"><img src="/wordpress/wp-content/themes/hyperion_theme1/assets/images/hyperion-logo.svg"/></div>
       </div>  
 
--- main.js
+++ main.js
@@ -6,8 +6,10 @@
 $(document).ready(function() {
 // $("[data-toggle=popover]").popover();
 
+
 $('.brand-box').popover();
 
+$(".popover-with-html").popover({ html : true });
 // $('.brand-box').click(function(){
     
     // $(this).append('<p>I\'m a paragraph</p>');
